570782dc9814ada7304f81f372ad48f1bf20374a
ertqaacapitalcom-creator <ertqaacapitalcom@gmail.com>
2026-04-08 00:09:51 +0200

    Update Google Tag Manager ID in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,12 +22,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TSZRZGSG');</script>
+})(window,document,'script','dataLayer','GTM-KSG948FH');</script>
 <!-- End Google Tag Manager -->
       </head>
       <body className={cairo.className}>
-   <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TSZRZGSG"
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KSG948FH"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
       </body>
